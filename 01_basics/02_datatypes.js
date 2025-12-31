dd1d4a7649e1f7ccfec965b9d6ffbdb7a4269e1d
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2025-12-31 03:06:39 +0000

    datatypes in javascript

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,6 +1,6 @@
 "use strictt" // treat all js code as newer version
 
-alert(3+3);  // cannnot open using nodejs we need browser 
+// alert(3+3);  // cannnot open using nodejs we need browser 
 console.log(3+3);
 console.log("atharv");
 
