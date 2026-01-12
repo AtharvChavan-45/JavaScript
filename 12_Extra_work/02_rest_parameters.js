2337abdd4c52975c37954a8ed9fba0428e1c8108
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2026-01-12 14:37:32 +0000

    what is rest parameters

---
+++ 12_Extra_work/02_rest_parameters.js
@@ -0,0 +1,29 @@
+// Rest parameters allow a function to accept any number of arguments 
+// and store them in an array.
+
+function sum(...numbers) { // we can add so many numbers
+  let total = 0;
+  for (let num of numbers) {
+    total += num;
+  }
+  return total;
+}
+
+console.log(sum(1, 2));        // 3
+console.log(sum(1, 2, 3, 4));  // 10
+
+
+// Example with fixed + rest parameters
+
+function showDetails(name, ...skills) {
+  console.log(name);
+  console.log(skills);
+}
+
+showDetails("Atharv", "JS", "React", "Flutter");
+
+// rest parameters in array function
+
+const multiply = (...nums) => nums.reduce((a, b) => a * b, 1);
+
+console.log(multiply(2, 3, 4)); // 24
