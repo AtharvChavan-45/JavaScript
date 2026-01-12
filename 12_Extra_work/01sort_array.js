448db14f8d50985351e1b209d3028d12337fea9a
Atharv Ashvin Chavan <158123475+AtharvChavan-45@users.noreply.github.com>
2026-01-12 14:37:58 +0000

    imp points sorting in javascript

--- 12_Extra_work/01sort_array.js
+++ 12_Extra_work/01sort_array.js
@@ -1,4 +1,5 @@
 const arr = [1,2,3,4,5,6];
+console.log(...arr);
 
 function maxProduct(arr){
     const sortedArr = arr.sort((a,b)=>b-a);
@@ -6,3 +7,28 @@ function maxProduct(arr){
 }
 const answer =  maxProduct(arr);
 console.log(answer);
+
+// delete all element in array with the help of one line
+// arr.length = 0;
+ arr.splice(0,arr.length);
+console.log(...arr);
+
+// merge two object 
+const obj1 = {
+    title : 'atharv',
+    age : 18
+}
+
+const obj2 = {
+    title : 'raj',
+    age : 20
+}
+
+const obj3 = {...obj1,...obj2}; // best option
+
+const mergedObj = {
+  obj1,
+  obj2
+};
+
+console.log(obj3);
